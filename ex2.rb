b9fff2a0aa30eeadb87b871456c4586c0fd3ec72
Sven Bold <svenbold@gmail.com>
2019-02-11 15:51:35 +0000

    ex2 extra credit

--- ex2.rb
+++ ex2.rb
@@ -3,3 +3,7 @@
 puts "I could have code like this" # And invisible gubbins like this
 #puts "This won't run"
 puts "This will run"
+#puts "This won't run again"
+puts "This is not a pepetual motion machine"
+
+
