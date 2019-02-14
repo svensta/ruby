e5a004d69f61bc51c87e62a1f2826cf18c33db8b
Sven Bold <svenbold@gmail.com>
2019-02-14 20:05:54 +0000

    commiting more ex5

--- ex5.rb
+++ ex5.rb
@@ -16,3 +16,7 @@ puts "He has got %s eyes and %s hair." % [MY_EYES, MY_HAIR]
 puts "His teeth are %s most of the time." % MY_TEETH
 
 puts "If I add %d, %d and %d I get %d." %[MY_AGE, MY_HEIGHT, MY_WEIGHT, MY_AGE + MY_HEIGHT + MY_WEIGHT]
+
+COCK = 12
+CONVERSION = 2.54
+puts "My %d inch penis (when limp btw) is just over %d metric centimeters" %[COCK, COCK * CONVERSION]
