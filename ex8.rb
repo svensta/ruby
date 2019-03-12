c6f399076f88fbb6f628c4f5d84f40377bdc39e7
Sven Bold <svenbold@gmail.com>
2019-03-12 15:54:47 +0000

    More printing and yet more

--- ex8.rb
+++ ex8.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 
 formatter = "%s %s %s %s"
+spacing = "%s %s %s %s %s %s"
 
 puts formatter %[1, 2, 3, 4]
 puts formatter %["one", "two", "three", "four"]
@@ -12,3 +13,17 @@ puts formatter %[
 		"But it didn't sing.",
 		"So I said goodnight."
 ]
+
+
+
+puts spacing %["one", "two", "three", "four", "five", "six"]
+puts spacing %[6, 5, 4, 3, 2, 1]
+puts spacing %[
+	"I eat peas with honey",
+	"I have done this all my life",
+	"It make the peas taste funny",
+	"Buit it keeps them on my knife",
+	"UNIX for dummies",
+	"Circa 1998/1999"
+]
+	
