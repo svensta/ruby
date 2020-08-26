a89f2451275815d1d80bcc6de6f6d0be6fe2402b
svensta <user@localhost>
2020-08-26 12:51:35 +0100

    Wed Aug 26 12:51:22 BST 2020

---
+++ ex11.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/ruby
+
+print "How old are you?"
+age = gets.chomp()
+print "How tall are you in cm?"
+height = gets.chomp()
+print "How much do you weigh?"
+weight = gets.chomp()
+
+puts "So, you weigh #{weight} kilos, you are #{height} centimeters tall and #{age} years old"
---
+++ ex9.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "here are the days.", days
+puts "here are the months.", months
+puts "here are the days #{days}."
+puts "here are the months#{months}."
+
+puts <<PARAGRAPH
+There's something going on here.
+with the PARAGRAPH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
